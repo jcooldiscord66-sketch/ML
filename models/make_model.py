8bdb3c8548cae8c73f5fd43e6876ec3956d294bb
jcooldiscord66-sketch <jcooldiscord66@gmail.com>
2025-10-19 10:01:31 +0530

    Refactor linear regression and add decision tree classes

--- models/make_model.py
+++ models/make_model.py
@@ -2,19 +2,16 @@ from collections import Counter
 import numpy as np
 import math
 from ML.encoders import OneHotEncoder
-from pandas import *  
-class RegressionError(Exception):
-    pass
+import sys
+sys.setrecursionlimit(10000)
 class linearRegression:
-    def fit(self,data:dict):
-        if not isinstance(data, dict) or "X" not in data or "y" not in data:
-            raise RegressionError("Input must be a dictionary with keys 'X' and 'y'")
-        assert len(data["X"]) == len(data["y"]),"the data must be of same length"
-        feature_matrix = np.array(data["X"])
+    def fit(self,X,y):
+        assert len(X) == len(y),"the data must be of same length"
+        feature_matrix = np.array(X)
         n_samples = feature_matrix.shape[0]
         bias_column = np.ones((n_samples,1))
         feature_matrix = np.hstack([bias_column,feature_matrix])
-        target_vector = np.array(data["y"]).reshape(-1,1)
+        target_vector = np.array(y).reshape(-1,1)
         transpose = feature_matrix.transpose()
         mul = transpose @ feature_matrix
         inverse = np.linalg.pinv(mul)
@@ -28,6 +25,22 @@ class linearRegression:
             prediction += i*j
         prediction += self.intercept
         return prediction
+class MultipleOutputLinearRegression:
+    def fit(self,X,y):
+        models = {}
+        each_model_data = []
+        for i in zip(*y):
+            each_model_data.append(i)
+        for j in range(len(each_model_data)):
+            models.update({j:linearRegression()})
+            models[j].fit(X,each_model_data[j])
+        self.models = models
+    def predict(self,value):
+        predictions = []
+        for i in self.models:
+            predictions.append(self.models[i].predict(value))
+        return predictions
+        
 def mean(v):
     return sum(v)/len(v)
 def std(p):
@@ -145,3 +158,217 @@ class KNN:
             w = Counter(answers)
             winner = w.most_common()
             return winner[0]
+class DecisionTreeClassifier:
+    def __build_tree__(self,X,y,method="Equality",criterion="impurity"):
+        unique = list(dict.fromkeys(y))
+        total = len(y)
+        proportions = []
+        for i in unique:
+            t = y.count(i)
+            proportions.append(t/total)
+        if criterion=="impurity":
+            G_main = 1 -sum(p**2 for p in proportions)
+        else:
+            G_main = -sum(p*math.log2(p) for p in proportions)
+        features = []
+        for j in zip(*X):
+            features.append(j)
+        uniques = []
+        for k in features:
+            uniques.append(list(dict.fromkeys(k)))
+        best_gain = -1
+        best_feature_index = None
+        best_split_value = None
+        best_X_psplit = None
+        best_y_psplit = None
+        best_X_qsplit = None
+        best_y_qsplit = None
+        for f_idx,(i,j) in enumerate(zip(features,uniques)):
+            for n in j:
+                p = []
+                q = []
+                for ij,kl in enumerate(i):
+                    if method == "Equality":
+                        if kl == n:
+                            p.append(ij)
+                        else:
+                            q.append(ij)
+                    else:
+                        if kl <= n:
+                            p.append(ij)
+                        else:
+                            q.append(ij)
+                X_psplit = [X[ml]for ml in p]
+                y_psplit = [y[nl]for nl in p]
+                X_qsplit = [X[bn]for bn in q]
+                y_qsplit = [y[something]for something in q]
+                p_unique = list(dict.fromkeys(y_psplit))
+                q_unique = list(dict.fromkeys(y_qsplit))
+                p_prop = []
+                q_prop = []
+                for s in p_unique:
+                    count = y_psplit.count(s)
+                    p_prop.append(count/len(p))
+                for t in q_unique:
+                    c = y_qsplit.count(t)
+                    q_prop.append(c/len(q))
+                if criterion=="impurity":
+                    p_gini = 1 - sum(proportion**2 for proportion in p_prop)
+                    q_gini = 1 - sum(propo**2 for propo in q_prop)
+                else:
+                    p_gini = -sum(proportion*math.log2(proportion)for proportion in p_prop)
+                    q_gini = -sum(prop*math.log2(prop)for prop in q_prop)
+                tota = len(p)+len(q)
+                g_weight = (len(p)/tota)*p_gini + (len(q)/tota)*q_gini
+                gini_gain = G_main - g_weight
+                if gini_gain > best_gain:
+                    best_gain = gini_gain
+                    best_X_psplit =X_psplit
+                    best_X_qsplit = X_qsplit
+                    best_y_psplit =  y_psplit
+                    best_y_qsplit = y_qsplit
+                    best_feature_index = f_idx
+                    best_split_value = n      
+        nodes = {}   
+        if best_gain<=0:
+            v = Counter(y)
+            value = v.most_common()[0]
+            nodes.update({"leaf":value[0]})
+        else:
+            left = self.__build_tree__(best_X_psplit,best_y_psplit,method)
+            right = self.__build_tree__(best_X_qsplit,best_y_qsplit,method)
+            nodes.update({"left":left,
+                          "right":right,
+                          "feature_index":best_feature_index,
+                          "split_value":best_split_value})
+            return nodes
+    def  fit(self,X,y,method="Equality",criterion="impurity"):
+        self.tree = self.__build_tree__(X,y,method,criterion=criterion)
+        self.method = method
+    def _predict_sample_(self,X,node):
+        if "leaf" in node:
+            return node["leaf"]
+        else:
+            feature_index = node["feature_index"]
+            split_value = node["split_value"]
+            raja = X[feature_index]
+            if self.method == "Equality":
+                if raja == split_value:
+                    return self._predict_sample_(X,node["left"])
+                else:
+                    return self._predict_sample_(X,node["right"])
+            else:
+                if raja <= split_value:
+                    return self._predict_sample_(X,node["left"])
+                else:
+                    return self._predict_sample_(X,node["right"])
+    def predict(self,X):
+        answers = []
+        for i in X:
+            ans = self._predict_sample_(i,self.tree)
+            answers.append(ans)
+        return answers
+class DecisionTreeRegression:
+    def __build_tree__(self,X,y,method="Equality",criterion="variance"):
+        if criterion=="variance":
+            G_main = std(y)**2
+        else:
+            m = mean(y)
+            l = []
+            for each_value in y:
+                l.append((each_value-m)**2)
+            G_main = mean(l)
+        features = []
+        for j in zip(*X):
+            features.append(j)
+        uniques = []
+        for k in features:
+            uniques.append(list(dict.fromkeys(k)))
+        best_gain = -1
+        best_feature_index = None
+        best_split_value = None
+        best_X_psplit = None
+        best_y_psplit = None
+        best_X_qsplit = None
+        best_y_qsplit = None
+        for f_idx,(i,j) in enumerate(zip(features,uniques)):
+            for n in j:
+                p = []
+                q = []
+                for ij,kl in enumerate(i):
+                    if method == "Equality":
+                        if kl == n:
+                            p.append(ij)
+                        else:
+                            q.append(ij)
+                    else:
+                        if kl <= n:
+                            p.append(ij)
+                        else:
+                            q.append(ij)
+                X_psplit = [X[ml]for ml in p]
+                y_psplit = [y[nl]for nl in p]
+                X_qsplit = [X[bn]for bn in q]
+                y_qsplit = [y[something]for something in q]
+                tota = len(p)+len(q)
+                if criterion=="variance":
+                    p_gain = std(y_psplit)**2
+                    q_gain = std(y_qsplit)**2
+                else:
+                    average_p = mean(y_psplit)
+                    average_q = mean(y_qsplit)
+                    list_for_p = []
+                    list_for_q = []
+                    for each_number in p:
+                        list_for_p.append((each_number-average_p)**2)
+                    p_gain = sum(list_for_p)/len(list_for_p)
+                    for each_number in q:
+                        list_for_q.append((each_number-average_q)**2)
+                    q_gain = sum(list_for_q)/len(list_for_q)
+                g_weight = (len(p)/tota)*p_gain + (len(q)/tota)*q_gain
+                gain = G_main - g_weight
+                if gain > best_gain:
+                    best_gain = gain
+                    best_X_psplit =X_psplit
+                    best_X_qsplit = X_qsplit
+                    best_y_psplit =  y_psplit
+                    best_y_qsplit = y_qsplit
+                    best_feature_index = f_idx
+                    best_split_value = n      
+        nodes = {}   
+        if best_gain<=0:
+            return mean(y)
+        else:
+            left = self.__build_tree__(best_X_psplit,best_y_psplit,method)
+            right = self.__build_tree__(best_X_qsplit,best_y_qsplit,method)
+            nodes.update({"left":left,
+                          "right":right,
+                          "feature_index":best_feature_index,
+                          "split_value":best_split_value})
+            return nodes
+    def  fit(self,X,y,method="Equality",criterion="variance"):
+        self.tree = self.__build_tree__(X,y,method,criterion=criterion)
+        self.method = method
+    def _predict_sample_(self,X,node):
+        if "leaf" in node:
+            return node["leaf"]
+        else:
+            feature_index = node["feature_index"]
+            split_value = node["split_value"]
+            raja = X[feature_index]
+            if self.method == "Equality":
+                if raja == split_value:
+                    return self._predict_sample_(X,node["left"])
+                else:
+                    return self._predict_sample_(X,node["right"])
+            else:
+                if raja <= split_value:
+                    return self._predict_sample_(X,node["left"])
+                else:
+                    return self._predict_sample_(X,node["right"])
+    def predict(self,X):
+        answers = []
+        for i in X:
+            ans = self._predict_sample_(i,self.tree)
+            answers.append(ans)
+        return answers
